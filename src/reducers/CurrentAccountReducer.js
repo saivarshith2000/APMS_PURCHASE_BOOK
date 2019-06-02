358bac530f9d2a2b55931689bce9a69f12b778bf
saivarshith2000 <hosvarshith@gmail.com>
2019-06-02 20:52:37 +0530

    fixed bug where current account balance is not updated on new purchase.

--- src/reducers/CurrentAccountReducer.js
+++ src/reducers/CurrentAccountReducer.js
@@ -1,8 +1,20 @@
 import * as types from "../types";
 
-export default (state = {}, action) => {
-  if (action.type === types.SET_CURRENT_ACCOUNT) {
-    return action.payload;
+export default (state = {}, { type, payload }) => {
+  switch (type) {
+    case types.SET_CURRENT_ACCOUNT:
+      return payload;
+    case types.ADD_NEW_TRANSACTION:
+      // we need to update the current account details when a new transaction is made
+      // a transaction is made only in the current account, so we just need to update the
+      // current balance accordingly
+      return {
+        ...state,
+        balance:
+          payload.type === types.ADD_MONEY
+            ? state.balance + parseFloat(payload.amount)
+            : state.balance - parseFloat(payload.amount)
+      };
   }
   return state;
 };
